90843c8eda80ec4f685fdea90032a35b1fc12ac4
jricher <jricher@cisco.com>
2022-07-17 21:14:55 +0100

    Optimised docker build and push script and added colour page to hello world

--- buildandpush.sh
+++ buildandpush.sh
@@ -8,12 +8,66 @@
 
 
 DIR=/Users/jricher/Projects/Joe-test-project
-FILE=/Users/jricher/Projects/Joe-test-project
 VERSION=$(sudo docker images | awk '{print $2}' | awk 'NR==2')
-NEXTVERSION=$(echo ${VERSION} | awk -F. -v OFS=. '{$NF=$NF+1;print}')
 REGISTRY=localhost:5000
 
-echo "Start"
+#Helper function to increment the version number passed in by user. 
+
+function increment_ver() {
+    while getopts ":Mmp" Option
+    do
+      case $Option in
+        M ) major=true;;
+        m ) minor=true;;
+        p ) patch=true;;
+      esac
+    done     
+
+    shift $(($OPTIND - 1))
+
+    version=$1
+
+    # Build array
+
+    a=( ${version//./ } )
+
+    if [ ${#a[@]} -ne 3 ]
+    then
+      echo "usage: $(basename $0) [-Mmp] major.minor.patch"
+      exit 1
+    fi
+
+    # Increment version numbers
+
+    if [ ! -z $major ]
+    then
+      ((a[0]++))
+      a[1]=0
+      a[2]=0
+    fi
+
+    if [ ! -z $minor ]
+    then
+      ((a[1]++))
+      a[2]=0
+    fi
+
+    if [ ! -z $patch ]
+    then
+      ((a[2]++))
+    fi
+
+    echo "${a[0]}.${a[1]}.${a[2]}"
+}
+
+level=$1
+num=$2
+
+if [ $# -eq 0 ]; then
+ echo "No arguments given, provide level of release and current version. Example: ./buildandpush.sh -p 1.1.1"
+ exit 1
+fi
+
 echo "Check build directory exists"
 if [ -d "$DIR" ];
 then
@@ -41,22 +95,35 @@ echo "Something went wrong with the current tag, exiting"
 exit 1
 fi
 
-echo "The next version will be $NEXTVERSION"
+VER=$(increment_ver $level $num) 
 
-TAG=$(echo "$NEXTVERSION")
+while true; do
+
+read -p "The next version selected is $VER. Do you want to proceed? (y/n) " yn
+
+case $yn in 
+    [yY] ) echo ok, begin docker build;
+        break;;
+    [nN] ) echo exiting...;
+        exit;;
+    * ) echo invalid response;
+        exit 1;;
+esac
+
+done
 
 echo "Build the docker image in the current directory"
-result=$(sudo docker build -t $REGISTRY/hello:$(echo $TAG) .) 
+result=$(sudo docker build -t $REGISTRY/hello:$VER .) 
 
 if [ $? -eq 0 ]; then
-   echo "Docker build was OK, new image is: $REGISTRY/hello:$(echo $TAG)"
+   echo "Docker build was OK, new image is: $REGISTRY/hello:$VER"
 else
    echo "Docker build failed, exiting"
    exit 1 
 fi
 
 echo "Pushing new docker image to local registry: $REGISTRY"
-result=$(sudo docker push $REGISTRY/hello:$NEXTVERSION) 
+result=$(sudo docker push $REGISTRY/hello:$VER) 
 if [ $? -eq 0 ]; then
    echo "Docker push was OK"
 else
--- task1-hello.py
+++ task1-hello.py
@@ -1,10 +1,26 @@
-from flask import Flask 
-
+from flask import Flask
+ 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Hello world!"
+
+@app.route("/colour")
+def colour():
+    user = {
+        'name':'Joe'
+    }
+    return '''
+    <html>
+    <head>
+    <title>Colorful Hello, World!</title>
+    </head>
+    <body>
+    <h1 style="color:#093657"> Hello '''+user['name']+'''!</h1>
+    </body>
+    </html>
+    '''
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
